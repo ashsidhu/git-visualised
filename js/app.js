df2811a8a0801a2dd2c72ad2c57b29dcaf842640
Ashmeet Sidhu <ashmeet.sidhu7@gmail.com>
2014-11-20 21:40:17 -0800

    c3 charts

--- js/app.js
+++ js/app.js
@@ -3,13 +3,31 @@ require.config({
   paths: {
     "jquery": ["../bower_components/jquery/dist/jquery.min"],
     "underscore": "../bower_components/underscore/underscore-min",
-    "d3": "../bower_components/d3/d3"
+    "d3": "../bower_components/d3/d3",
+    "c3": "../bower_components/c3/c3"
+  },
+  shim: {
+    "c3": {
+            //loads dependencies first
+      deps: ["d3"],
+            //custom export name, this would be lowercase otherwise
+      exports: "c3"
+    }
   }
 });
 
 //requiring the scripts in the first argument and then passing the library namespaces into a callback
 //you should be able to console log all of the callback arguments
-var app = require(['jquery', 'underscore', 'd3'], function(jquery, _, d3){
+require(['jquery', 'underscore', 'd3', 'c3'], function(jquery, _, d3, c3){
   console.log('init');
-  // return app = new App;
+
+  var chart = c3.generate({
+      bindto: '#chart',
+      data: {
+        columns: [
+          ['data1', 30, 200, 100, 400, 150, 250],
+          ['data2', 50, 20, 10, 40, 15, 25]
+        ]
+      }
+  });
 });
